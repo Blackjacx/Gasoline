725fb8656c4c3837e95a10239c0e9443a380b2cf
Stefan Herold <stefan.herold@gmail.com>
2017-07-06 01:26:14 +0200

    implments codable

--- Gasoline/User.swift
+++ Gasoline/User.swift
@@ -8,17 +8,23 @@
 
 import UIKit
 
-public struct User: CoreModel {
+public struct User: Codable {
 
-    struct JsonKey {
-        static let email = "email"
-        static let firstName = "first_name"
-        static let lastName = "last_name"
+    enum CodingKeys : String, CodingKey {
+        case id
+        case createdAt = "created_at"
+
+        case email
+        case firstName = "first_name"
+        case lastName = "last_name"
     }
 
-    /// The model with properties common to all model objects
-    public let common: Common
+    /// The database id for the model object
+    public let id: String
+    /// The creation date of the model object
+    public let createdAt: Date
 
+    
     /// The email address of the user
     public let email: String
     /// The first name of the user
