0b92eeb877262428388e3fcfd8674ae4cf9db08e
Stefan Herold <stefan.herold@gmail.com>
2017-07-09 18:11:09 +0200

    reduces todos to a minimum

--- Gasoline/AppDelegate.swift
+++ Gasoline/AppDelegate.swift
@@ -9,21 +9,11 @@
 // IMPROVEMENTS
 // TODO: Implement Adding
 // TODO: Implement Persistence
-// TODO: Use the decimal keypad and and include a accessory view on top with next prev buttons
-// TODO: Make fonts dark gray: 30, 60, ...
-// TODO: Realize Datepicker as seperate View and show it from below on button press.
-// TODO: Check the Date 22:00 after changing dateii
-// TODO: Integrate an advanced date formatter class and a relative date/time format on dashboard
 
 // FEATURES
 // TODO: Taking Picture of the bill
-// TODO: Use a scroll view to display the content
-// TODO: Convert Pictures to b&w images to save space
-// TODO: Offer to crop image to save space
 // TODO: OCR mileage in the car via camera
 // TODO: Implement NSNumberFormatter and reduce to 1 textfield for currency: http://stackoverflow.com/questions/276382/what-is-the-best-way-to-enter-numeric-values-with-decimal-points
-// TODO: Toggle Edit/Save depending on mode you are in
-// TODO: Make a login screen
 
 import UIKit
 
