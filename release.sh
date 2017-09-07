35c032fc0e14871f8278c189ebb65e069e88bf6e
Stefan Herold <stefan.herold@gmail.com>
2017-09-07 19:04:26 +0200

    Add release.sh and simplify Fastfile

---
+++ release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+# Iconizer shell script by Stefan Herold
+
+# This is a simple tool to generate all necessary app icon sizes from one vector file.
+# To use: specify the path to your vector graphic (PDF format)
+# Example: sh iconizer.sh MyVectorGraphic.pdf
+
+# Requires ImageMagick: http://www.imagemagick.org/
+# Requires jq: https://stedolan.github.io/jq/
+
+# set -e
+# set -x
+
+if [ $# -ne 1 ]
+  then
+        echo "Usage: $0 <version>"
+else
+    echo "Triggering CI to build product..."
+
+    curl https://www.bitrise.io/app/d4f39dc6286bb63a/build/start.json --data '{"hook_info":{"type":"bitrise","api_token":"Jx56IDVv9IaM83x6GE7hsw"},"build_params":{"branch":"develop","workflow_id":"github_release","environments":[{"mapped_to":"RELEASING_GIT_TAG","value":"'$(echo $1)'","is_expand":true}]},"triggered_by":"curl"}'
+fi
