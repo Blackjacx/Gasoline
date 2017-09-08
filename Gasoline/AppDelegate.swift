4edfa19e27c5fcb5efa35c24a586679d788d8be9
Stefan Herold <stefan.herold@gmail.com>
2017-09-08 11:09:59 +0200

    Add Crashlytics

--- Gasoline/AppDelegate.swift
+++ Gasoline/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 var globalDataBaseReference: DataBase = DataBaseImplementation.shared
 
@@ -18,6 +20,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        // Setup Crashlytics
+        Fabric.with([Crashlytics.self])
 
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.backgroundColor = UIColor.white
